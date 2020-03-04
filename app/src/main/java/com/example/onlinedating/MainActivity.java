d1308e788189712ffa85b3aee6a04b3d037880e4
navita-saini <navita.nextpage@gmail.com>
2020-03-04 08:36:35 +0530

    Initial Commit

--- app/src/main/java/com/example/onlinedating/MainActivity.java
+++ app/src/main/java/com/example/onlinedating/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
