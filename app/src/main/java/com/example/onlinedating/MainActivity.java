abe926ac658ca6e5e1da523134dc3e7eb10a5196
navita-saini <navita.nextpage@gmail.com>
2020-03-04 08:32:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/onlinedating/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.onlinedating;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
